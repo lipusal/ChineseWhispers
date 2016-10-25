5ad5d79613862e41988dcf5fd640e1cb6737cb7e
Diego de Rochebouet <drochebouet@outlook.com>
2016-10-25 17:43:26 -0300

    Added more to model. contents and attributes.

---
+++ chinese-whispers/application/src/main/java/ar/edu/itba/chinese_whispers/application/model/ExtendedContent.java
@@ -0,0 +1,15 @@
+package ar.edu.itba.chinese_whispers.application.model;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by dgrimau on 25/10/16.
+ */
+public class ExtendedContent extends  Content{
+
+    private List<ExtendedContent> extendedContents;
+
+
+
+}

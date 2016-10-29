cef1be817986c7da8b61b01095862dcb31405763
Juan Li Puma <juanlipuma94@gmail.com>
2016-10-28 23:17:12 -0300

    Improve XML parser implementation

--- chinese-whispers/xmpp/src/main/java/ar/edu/itba/chinese_whispers/xmpp/parser/XmlParser.java
+++ chinese-whispers/xmpp/src/main/java/ar/edu/itba/chinese_whispers/xmpp/parser/XmlParser.java
@@ -1,40 +1,32 @@
 package ar.edu.itba.chinese_whispers.xmpp.parser;
 
 import com.fasterxml.aalto.AsyncByteArrayFeeder;
-import com.fasterxml.aalto.AsyncByteBufferFeeder;
 import com.fasterxml.aalto.AsyncXMLInputFactory;
 import com.fasterxml.aalto.AsyncXMLStreamReader;
 import com.fasterxml.aalto.stax.InputFactoryImpl;
 
 import javax.xml.stream.XMLStreamException;
 import java.io.UnsupportedEncodingException;
-import java.nio.ByteBuffer;
-import java.util.function.Supplier;
+import java.util.Arrays;
 
 /**
- * Created by juanlipuma on Oct/24/16.
+ * Basic XML parser that interprets start and end of elements, and recognizes when more bytes are needed to complete
+ * an element, character, etc.
  */
-public class XmlParser implements /*Consumer<Byte[]>,*/ Supplier<String> {
-
-//    private final ReaderConfig config = new ReaderConfig();
-//
-//    private XMLStreamReader2 readerr = new StreamReaderImpl(new Utf8Scanner());
+public class XmlParser /*implements Consumer<Byte[]>, Supplier<String>*/ {
 
     private final AsyncXMLInputFactory inputFactory;
     private final AsyncXMLStreamReader<AsyncByteArrayFeeder> parser;
 
     private int status = 0;
-    private int depth = 0;
-
-    //TODO decide whether to use this
-    private AsyncXMLStreamReader<AsyncByteBufferFeeder> parser2;
+    private boolean isSilenced;
+    private boolean isL337ed = true;
+    private boolean isInBodyTag = false;
+    private StringBuilder readXML = new StringBuilder();
 
     public XmlParser() throws XMLStreamException {
         inputFactory = new InputFactoryImpl();
         parser = inputFactory.createAsyncForByteArray();
-
-        //TODO delete if unused
-        parser2 = inputFactory.createAsyncForByteBuffer();
     }
 
     public XmlParser(byte[] initialData) throws XMLStreamException {
@@ -47,10 +39,6 @@ public class XmlParser implements /*Consumer<Byte[]>,*/ Supplier<String> {
         parser.getInputFeeder().feedInput(data, 0, data.length);
     }
 
-    public void feed(ByteBuffer buffer) throws XMLStreamException {
-        parser2.getInputFeeder().feedInput(buffer);
-    }
-
     public boolean hasData() {
         try {
             return parser.hasNext() /*&& status != AsyncXMLStreamReader.EVENT_INCOMPLETE*/;
@@ -61,65 +49,76 @@ public class XmlParser implements /*Consumer<Byte[]>,*/ Supplier<String> {
         }
     }
 
-    public String nextStanza() {
+    public void parse() {
         if (!hasData()) {
-            return null;
+            return;
         }
-        boolean done = false;
-        while (hasData() && !done) {
+        while (hasData()) {
             next();
             switch (status) {
                 case AsyncXMLStreamReader.START_ELEMENT:
-                    depth++;
-                    System.out.print(parser.getName());
-                    for (int i = 0; i < parser.getAttributeCount(); i++) {
-                        System.out.print(parser.getAttributeName(i) + "=" + parser.getAttributeValue(i));
+                    if (parser.getLocalName().equals("body")) {
+                        isInBodyTag = true;
                     }
-                    try {
-                        if (parser.isEmptyElement()) {
-                            System.out.println("Empty element");
+                    readXML.append("<");
+                    if (!parser.getName().getPrefix().isEmpty()) {
+                        readXML.append(parser.getPrefix()).append(":");
+                    }
+                    readXML.append(parser.getLocalName());
+                    int attrCount = parser.getAttributeCount();
+                    if (attrCount > 0) {
+                        readXML.append(" ");
+                        for (int i = 0; i < attrCount; i++) {
+                            readXML.append(parser.getAttributeName(i)).append("=\"").append(parser.getAttributeValue(i)).append(i < attrCount - 1 ? "\" " : "\"");
                         }
-                    } catch (XMLStreamException e) {
-                        e.printStackTrace();
                     }
+                    readXML.append(">");
                     break;
                 case AsyncXMLStreamReader.CHARACTERS:
-                    System.out.print(parser.getText());
+                    if (isInBodyTag && isL337ed) {
+                        for (char c : parser.getTextCharacters()) {
+                            switch (c) {
+                                case 'a':
+                                    readXML.append("4");
+                                    break;
+                                case 'e':
+                                    readXML.append("3");
+                                    break;
+                                case 'i':
+                                    readXML.append("1");
+                                case 'o':
+                                    readXML.append("0");
+                                    break;
+                                case 'c':
+                                    readXML.append("&lt;");
+                                    break;
+                                default:
+                                    readXML.append(c);
+                                    break;
+                            }
+                        }
+                    } else {
+                        readXML.append(parser.getText());
+                    }
                     break;
                 case AsyncXMLStreamReader.END_ELEMENT:
-                    if (--depth == 0) {
-                        done = true;
+                    if (parser.getLocalName().equals("body")) {
+                        isInBodyTag = false;
                     }
-                    System.out.println("End " + parser.getName());
+                    readXML.append("</");
+                    if (!parser.getName().getPrefix().isEmpty()) {
+                        readXML.append(parser.getPrefix()).append(":");
+                    }
+                    readXML.append(parser.getLocalName());
+                    readXML.append(">");
                     break;
                 case AsyncXMLStreamReader.EVENT_INCOMPLETE:
-                    done = true;
-                    return null;
+                    return;
                 case -1:
-                    return "Error";
+                    System.out.println("EXPLOTAR POR LOS AIRES!");
+                    return;
             }
         }
-
-        return null;
-//        QName qualifiedName = parser.getName();
-//        System.out.println("Read " + qualifiedName.getLocalPart() + " element");
-//        return qualifiedName.getLocalPart();
-    }
-
-    /*@Override
-    public void accept(Byte[] bytes) {
-        feed(bytes);
-    }*/
-
-    @Override
-    public String get() {
-        return null;
-    }
-
-    public static void main(String[] args) throws XMLStreamException, UnsupportedEncodingException {
-        XmlParser p = new XmlParser("<root caca='caca'><inner>HELLO WORLD</inner><empty /></root>".getBytes());
-        System.out.println(p.nextStanza());
-        System.out.println(p.nextStanza());
     }
 
     private int next() {
@@ -130,5 +129,47 @@ public class XmlParser implements /*Consumer<Byte[]>,*/ Supplier<String> {
             status = -1;
         }
         return status;
+    }
+
+    public static void main(String[] args) throws XMLStreamException, UnsupportedEncodingException {
+        XmlParser parser = new XmlParser("<?xml version=\"1.0\" encoding=\"UTF-8\" ?><message><body>hola ke ase me gusta mucho la papa y comer todos los días es sano l0l0l0lolololol</body></message>".getBytes());
+        parser.parse();
+        System.out.println(parser.readXML.toString());
+        parser.parse();
+        System.out.println(parser.readXML.toString());
+    }
+
+    /**
+     * @return Whether this stream is silenced. Silenced streams discard all <message> stanzas.
+     */
+    public boolean isSilenced() {
+        return isSilenced;
+    }
+
+    /**
+     * Sets whether this stream is silenced. Silenced streams discard all <message> stanzas.
+     *
+     * @param silenced Whether this stream is silenced.
+     */
+    public void setSilenced(boolean silenced) {
+        isSilenced = silenced;
+    }
+
+    /**
+     * @return Whether this stream is "leeted." Leeted streams transform certain alphabetic characters inside <body>
+     *          stanzas into similar-looking numbers.
+     */
+    public boolean isL337ed() {
+        return isL337ed;
+    }
+
+    /**
+     * Sets whether this stream is "leeted." Leeted streams transform certain alphabetic characters inside <body>
+     * stanzas into similar-looking numbers.
+     *
+     * @param l337ed Whether this stream is leeted.
+     */
+    public void setL337ed(boolean l337ed) {
+        isL337ed = l337ed;
     }
 }
